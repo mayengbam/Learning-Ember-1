599dca5dda6f97e6042da9dfd573bd0528c4b010
Sushil Mayengbam <memsana.sushil@gmail.com>
2017-03-07 01:41:19 +0530

    Step3: Added static routes 1. ember g route orgs 2. ember g route org 3. ember g route org/repos 4. ember g route org/repo 5. ember g route org/repo/issues 6. ember g route org/repo/contributors

--- app/router.js
+++ app/router.js
@@ -7,6 +7,14 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('orgs');
+  this.route('org', {path: '/:id'}, function() {
+    this.route('repos');
+    this.route('repo', {path: '/:repoid'},function() {
+      this.route('issues');
+      this.route('contributors');
+    });
+  });
 });
 
 export default Router;
