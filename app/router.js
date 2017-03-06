bf95f0044de64a83ced1578a89c2304a471a9cd7
Sushil Mayengbam <memsana.sushil@gmail.com>
2017-03-07 03:28:39 +0530

    Step4: Added routes transitioning logic 1. ember g route index 2. ember g route org/index 3. ember g route org/repo/index

--- app/router.js
+++ app/router.js
@@ -7,14 +7,15 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('orgs');
-  this.route('org', {path: '/:id'}, function() {
-    this.route('repos');
+  this.route('orgs', {});
+  this.route('org', {path: 'org/:id'}, function() {
+    this.route('repos', {});
     this.route('repo', {path: '/:repoid'},function() {
       this.route('issues');
       this.route('contributors');
     });
   });
+  this.route('notfound', {path: '*path'});
 });
 
 export default Router;
--- app/routes/orgs.js
+++ app/routes/orgs.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  activate(){
+    //console.log('Enter "orgs" route ...')
+  },
+  deactivate(){
+    //console.log('Exit "orgs" route ...')
+  }
 });
